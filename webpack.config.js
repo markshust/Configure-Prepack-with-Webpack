db02caf96c6c20e1b4f190959f0e93be036c95fd
Mark Shust <mark@shust.com>
2017-05-04 09:05:31 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const PrepackWebpackPlugin = require('prepack-webpack-plugin').default;
+
+module.exports = { 
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new PrepackWebpackPlugin()
+  ]
+};
+
